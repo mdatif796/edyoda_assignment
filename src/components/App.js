d4c9ea88ade7448c69cd8c6ed2b31c0ef3927a16
mdatif796 <mdatif796@gmail.com>
2023-03-30 12:54:17 +0530

    created program container

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import DropdownContainer from "./DropdownContainer";
+import Navbar from "./Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <DropdownContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ProgramContainer.js
@@ -0,0 +1,29 @@
+import styles from "../styles/programContainer.module.css";
+import { dropdown } from "../data/dropdown";
+import { useState } from "react";
+
+const ProgramContainer = ({ handleActiveCourse, activeCourse }) => {
+  const handleClick = (e) => {
+    handleActiveCourse(e.target.innerText);
+  };
+  return (
+    <div className={styles.programContainer}>
+      <p className={styles.programHeading}>Select Program</p>
+      <div className={styles.program}>
+        {dropdown.map((data, index) => {
+          return (
+            <p
+              key={index}
+              onClick={handleClick}
+              className={activeCourse === data ? styles.active : styles.para}
+            >
+              {data}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProgramContainer;
