9795a163b6b2002f67d41818ffcc240491edc74b
mdatif796 <mdatif796@gmail.com>
2023-03-31 19:31:37 +0530

    fix some styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,24 +3,42 @@ import { GoFileSubmodule } from "react-icons/go";
 import { GiTeacher } from "react-icons/gi";
 import styles from "../styles/menu.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Menu = () => {
+  const [currentPage, setCurrentPage] = useState("HOME");
+  const handleClick = (page) => {
+    setCurrentPage(page);
+  };
   return (
     <div className={styles.menuContainer}>
       <Link to="/">
-        <div className={styles.menu}>
+        <div
+          onClick={() => handleClick("HOME")}
+          className={currentPage === "HOME" ? styles.clickedMenu : styles.menu}
+        >
           <IoIosHome />
           <p>HOME</p>
         </div>
       </Link>
       <Link to="/module">
-        <div className={styles.menu}>
+        <div
+          onClick={() => handleClick("MODULES")}
+          className={
+            currentPage === "MODULES" ? styles.clickedMenu : styles.menu
+          }
+        >
           <GoFileSubmodule />
           <p>MODULES</p>
         </div>
       </Link>
       <Link to="/instructor">
-        <div className={styles.menu}>
+        <div
+          onClick={() => handleClick("INSTRUCTORS")}
+          className={
+            currentPage === "INSTRUCTORS" ? styles.clickedMenu : styles.menu
+          }
+        >
           <GiTeacher />
           <p>INSTRUCTORS</p>
         </div>
